2b67c0aafa543145c1d91d96b6533955c8824cf0
Tehho Work Laptop <Andreas.Antonsson@collectorbank.se>
2018-05-21 14:44:31 +0200

    Fixat med Word och twitterprofil

--- TwitterBot.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TwitterBot.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ TwitterBot.Domain/TwitterProfile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TwitterBot.Domain
+{
+    public class TwitterProfile
+    {
+        public string Name { get; set; }
+
+        private List<Tweet> _tweets { get; set; }
+
+        public TwitterProfile()
+        {
+
+        }
+
+        public Dictionary<string, int> GenerateMetaData()
+        {
+            var regex = new Regex("/(\\.| |!|\\?)/");
+
+            var dicReturn = new Dictionary<string, int>();
+
+            _tweets.ForEach(tweet =>
+            {
+                var list = regex.Split(tweet.Message).ToList();
+
+                list.Where(string.IsNullOrWhiteSpace).ToList().ForEach(word =>
+                {
+                    if (dicReturn.ContainsKey(word))
+                        dicReturn[word]++;
+                    else
+                        dicReturn.Add(word, 1);
+                });
+
+            });
+
+
+            return dicReturn;
+        }
+    }
+}
