6c673e53b2721942be666042a32ae5dfde4134b8
collectoranton <anton.lilja@collectorbank.se>
2018-05-21 15:57:41 +0200

    Broke out Token

---
+++ TwitterBot.Domain/Token.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterBot.Domain
+{
+    public class Token
+    {
+        public string key { get; set; }
+        public string secret { get; set; }
+    }
+}
