97e03989f62a3c34e895358f138e92f25d05206a
Tehho Work Laptop <Andreas.Antonsson@collectorbank.se>
2018-05-21 14:47:59 +0200

    Dbsäkrat TwitterProfile

--- TwitterBot.Domain/TwitterProfile.cs
+++ TwitterBot.Domain/TwitterProfile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -8,8 +9,11 @@ namespace TwitterBot.Domain
 {
     public class TwitterProfile
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-
+        [Required]
         private List<Tweet> _tweets { get; set; }
 
         public TwitterProfile()
