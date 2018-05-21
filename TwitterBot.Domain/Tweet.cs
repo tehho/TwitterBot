e71c54696e12589a1b162df6ad9842a5c5e57880
Unknown <mattiasj@protonmail.com>
2018-05-21 14:49:03 +0200

    La till en Tweetclass

---
+++ TwitterBot.Domain/Tweet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tweetinvi;
+
+namespace TwitterBot.Domain
+{
+    class Tweet
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+
+        public int UserId { get; set; }
+        public TwitterProfile User { get; set; }
+
+
+    }
+}
