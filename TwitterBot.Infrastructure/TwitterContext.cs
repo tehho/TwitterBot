c41561372ef40a8d0da3f82d5fb37eb3fc868188
Amanda Westerlund <Westerlund922@gmail.com>
2018-05-22 11:24:57 +0200

    SQLite uppe. Gjort TwitterContext

---
+++ TwitterBot.Infrastructure/TwitterContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TwitterBot.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace TwitterBot.Infrastructure
+{
+    public class TwitterContext : DbContext
+    {
+        public DbSet<TwitterProfile> TwitterProfiles { get; set; }
+
+        public TwitterContext(DbContextOptions<TwitterContext> context) : base(context)
+        {
+        }
+    }
+}
