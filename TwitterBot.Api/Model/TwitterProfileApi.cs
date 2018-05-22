3a0edec260da0ded256333b3095827ac32c9095a
Tehho Work Laptop <Andreas.Antonsson@collectorbank.se>
2018-05-22 11:55:54 +0200

    Fixat med hemsidan och endpoints

---
+++ TwitterBot.Api/Model/TwitterProfileApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.JsonPatch.Internal;
+using TwitterBot.Domain;
+
+namespace TwitterBot.Api.Model
+{
+    public class TwitterProfileApi
+    {
+        public string Name { get; set; }
+        public List<IStatus> _tweets { get; set; }
+
+        public TwitterProfileApi()
+        {
+            Name = "";
+            _tweets = new List<IStatus>();
+        }
+
+        public TwitterProfile ToDomain()
+        {
+            return new TwitterProfile
+            {
+                Name = Name,
+                _tweets = _tweets.ToList()
+            };
+        }
+    }
+}
