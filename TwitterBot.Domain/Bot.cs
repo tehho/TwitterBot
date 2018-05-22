ad82d20a623510719e15fa88ccf1c208e2b22592
collectoranton <anton.lilja@collectorbank.se>
2018-05-22 10:17:39 +0200

    Bot fix

--- TwitterBot.Domain/Bot.cs
+++ TwitterBot.Domain/Bot.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TwitterBot.Domain
 {
     public class Bot : Entity
     {
-        private List<Word> words;
-        public List<Profile> profiles;
+        private Dictionary<Profile, int> profiles;
 
+        [Required]
         public string Name { get; set; }
+        
+        public Bot(string name)
+        {
+            Name = name;
+            profiles = new Dictionary<Profile, int>();
+        }
     }
 }
